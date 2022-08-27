b7a24ef71e58388a76469e77beae8c62012d045f
eshwar360 <48169990+eshwar360@users.noreply.github.com>
2022-08-27 16:19:22 +0530

    Delete EmployeePerformanceProblemStatement.py

--- EmployeePerformanceProblemStatement.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_excel(r"C:\Users\eshwar\Downloads\Hackathon_Timesheet.xlsx")
-print(df.groupby(['Team','Project Name'])['Hours'].mean().rename('Mean').to_frame())
-Employee_with_low_efficiency=df.sort_values(['Hours'],ascending=[False]).head()
-print(Employee_with_low_efficiency)
-
